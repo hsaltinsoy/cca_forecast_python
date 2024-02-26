2cc9e9c8acf86b9304843fbb792f98e4746853b4
saidaltinsoy <said.altinsoy@kayenta.io>
2024-02-26 15:41:01 +0000

    added Summary class

--- src/forecast.py
+++ src/forecast.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from src.model import Entry
+from src.model import Entry, Summary
 
 
 def summarize_forecast(data):
@@ -14,30 +14,21 @@ def summarize_forecast(data):
 
     # Process each day
     for day, entries in group_day.items():
-        morning_temperature, morning_rain, afternoon_temperature, afternoon_rain = [], [], [], []
+        summary = Summary(entries)
+        summary.process()
         all_temperatures = [entry.average_temperature for entry in entries]
-
-        for entry in entries:
-            # collect morning period entries
-            if 6 <= entry.date_time.hour < 12:
-                morning_temperature.append(entry.average_temperature)
-                morning_rain.append(entry.probability_of_rain)
-            # collection afternoon period entries
-            elif 12 <= entry.date_time.hour < 18:
-                afternoon_temperature.append(entry.average_temperature)
-                afternoon_rain.append(entry.probability_of_rain)
 
         summary = {
             # if no morning data, report insufficient data
-            "morning_average_temperature": "Insufficient forecast data" if not morning_temperature else round(
-                sum(morning_temperature) / len(morning_temperature)),
-            "morning_chance_of_rain": "Insufficient forecast data" if not morning_rain else round(
-                sum(morning_rain) / len(morning_rain), 2),
+            "morning_average_temperature": "Insufficient forecast data" if not summary.morning_temperature else round(
+                sum(summary.morning_temperature) / len(summary.morning_temperature)),
+            "morning_chance_of_rain": "Insufficient forecast data" if not summary.morning_rain else round(
+                sum(summary.morning_rain) / len(summary.morning_rain), 2),
             # if no afternoon data, report insufficient data
-            "afternoon_average_temperature": "Insufficient forecast data" if not afternoon_temperature else round(
-                sum(afternoon_temperature) / len(afternoon_temperature)),
-            "afternoon_chance_of_rain": "Insufficient forecast data" if not afternoon_rain else round(
-                sum(afternoon_rain) / len(afternoon_rain), 2),
+            "afternoon_average_temperature": "Insufficient forecast data" if not summary.afternoon_temperature else round(
+                sum(summary.afternoon_temperature) / len(summary.afternoon_temperature)),
+            "afternoon_chance_of_rain": "Insufficient forecast data" if not summary.afternoon_rain else round(
+                sum(summary.afternoon_rain) / len(summary.afternoon_rain), 2),
             "high_temperature": max(all_temperatures),
             "low_temperature": min(all_temperatures)
         }
--- src/model.py
+++ src/model.py
@@ -10,3 +10,24 @@ class Entry:
 
     def get_day(self) -> date:
         return self.date_time.date()
+
+
+class Summary:
+
+    def __init__(self, entries):
+        self.morning_temperature = []
+        self.afternoon_temperature = []
+        self.morning_rain = []
+        self.afternoon_rain = []
+        self.entries = entries
+
+    def process(self):
+        for entry in self.entries:
+            # collect morning period entries
+            if 6 <= entry.date_time.hour < 12:
+                self.morning_temperature.append(entry.average_temperature)
+                self.morning_rain.append(entry.probability_of_rain)
+            # collection afternoon period entries
+            elif 12 <= entry.date_time.hour < 18:
+                self.afternoon_temperature.append(entry.average_temperature)
+                self.afternoon_rain.append(entry.probability_of_rain)
